8c74c7a278e4cd558442f761d7c622c7fc52bd53
Florian Damhaut <flda@odoo.com>
2022-12-07 13:45:36 +0100

    Day 4 One-liner

--- 4AB.py
+++ 4AB.py
@@ -8,6 +8,8 @@ for line in lines:
     score += 1 if a1 <= b1 <= b2 <= a2 or b1 <= a1 <= a2 <= b2 else 0
 
 print(score)
+print(sum(list(map(lambda x: 1 if x[0] <= x[2] <= x[3] <= x[1] or x[2] <= x[0] <= x[1] <= x[3] else 0, list(map(lambda line: [int(x) for l in line.split(',') for x in l.split('-')], [line.strip() for line in open('4.in').readlines()]))))))
+
 
 with open('4.in') as f:
     lines = f.readlines()
@@ -19,3 +21,5 @@ for line in lines:
     score += 0 if a1 <= a2 < b1 <= b2 or b1 <= b2 < a1 <= a2 else 1
 
 print(score)
+
+print(sum(list(map(lambda x: 0 if x[1] < x[2] or x[3] < x[0] else 1, list(map(lambda line: [int(x) for l in line.split(',') for x in l.split('-')], [line.strip() for line in open('4.in').readlines()]))))))
