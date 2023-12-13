61519de7443042dd29df63c49a5b035ef04d7a11
Florian Damhaut <flda@odoo.com>
2023-12-13 13:19:02 +0100

    13

--- 2023/13AB.py
+++ 2023/13AB.py
@@ -1,3 +1,66 @@
 with open('13.in') as f:
     lines = f.readlines()
 lines = [line.strip() for line in lines]
+
+patterns, pattern = [], []
+for line in lines:
+    if not line:
+        patterns += [pattern]
+        pattern = []
+    else:
+        pattern += [line]
+patterns += [pattern]
+
+def transpose(pattern):
+    pattern = zip(*pattern)
+    return [''.join(line) for line in pattern]
+
+def check_pattern(pattern, n):
+    i = 0
+    while 0 <= n-i and n+1+i < len(pattern):
+        if pattern[n-i] == pattern[n+1+i]:
+            i += 1
+        else:
+            return False
+    return True
+
+def find(pattern):
+    for n, line in enumerate(pattern[:-1]):
+        if check_pattern(pattern, n):
+            return (n+1)*100
+    tp = transpose(pattern)
+    for n, line in enumerate(tp[:-1]):
+        if check_pattern(tp, n):
+            return (n+1)
+    return 0
+
+print(sum(find(pattern) for pattern in patterns))
+
+def find_diff(s1, s2):
+    return [i for i in range(len(s1)) if s1[i] != s2[i]]
+
+def find_smudge(pattern, n):
+    diff = False
+    i = 0
+    while 0 <= n-i and n+1+i < len(pattern):
+        d = find_diff(pattern[n - i], pattern[n + 1 + i])
+        if not d:
+            i += 1
+        elif len(d) == 1 and not diff:
+            i += 1
+            diff = True
+        else:
+            return False
+    return diff
+
+def find(pattern):
+    for n, line in enumerate(pattern[:-1]):
+        if find_smudge(pattern, n):
+            return (n+1)*100
+    tp = transpose(pattern)
+    for n, line in enumerate(tp[:-1]):
+        if find_smudge(tp, n):
+            return (n+1)
+    return 0
+
+print(sum(find(pattern) for pattern in patterns))
